2e0864f78fc3b077133d19ab9031e72a4523427c
Ernesto Ledesma <eeledesma@teco.com.ar>
2024-05-27 10:08:31 -0300

    [ADD]: hooks de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react'
+
 
 export const Formulario = () => {
+
+    const [ nombre, setNombre ] = useState('')
+    const [ propietario, setPropietario ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ fecha, setFecha ] = useState('')
+    const [ sintomas, setSintomas ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        // Validacion del Formulario
+         if( [nombre, propietario, email, fecha, sintomas].includes('') )  {
+            console.log('Todos los campos son obligatorios')
+          } else {
+            console.log('Hay almnos un campo vacio')
+          }
+    
+          console.log('Enviando Formulario')
+    }
+
   return (
     <div className="md:w-1/2 lg:w-2/5 mx-5">
      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -10,6 +32,7 @@ export const Formulario = () => {
       </p>
 
     <form 
+        onSubmit={ handleSubmit }
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
     >
         <div className="mb-5">
@@ -21,6 +44,8 @@ export const Formulario = () => {
                 type="text"
                 placeholder="Nombre de la Mascota"
                 className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                value={nombre}
+                onChange={ (e) => setNombre(e.target.value) }
             />  
         </div>
 
@@ -33,6 +58,8 @@ export const Formulario = () => {
                 type="text"
                 placeholder="Nombre del Propietario"
                 className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                value={propietario}
+                onChange={ (e) => setPropietario(e.target.value) }
             />  
         </div>
 
@@ -45,6 +72,8 @@ export const Formulario = () => {
                 type="email"
                 placeholder="Email Contacto Propietario"
                 className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                value={email}
+                onChange={ (e) => setEmail(e.target.value) }
             />  
         </div>
 
@@ -56,6 +85,8 @@ export const Formulario = () => {
                 id="alta"
                 type="date"
                 className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                value={fecha}
+                onChange={ (e) => setFecha(e.target.value)}
                
             />  
         </div>
@@ -68,6 +99,8 @@ export const Formulario = () => {
                 id="sintomas"
                 className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                 placeholder="Describe los Síntomas"
+                value={sintomas}
+                onChange={ (e) => setSintomas(e.target.value) }
              
             />
         </div>
@@ -76,6 +109,7 @@ export const Formulario = () => {
             type="submit"
             className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
             value="Agregar Paciente"
+
         />
     </form>
 </div>
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,8 +2,12 @@
 
 export const ListadoPacientes = () => {
   return (
-    <div className="w-1/2">
-      <h2>Listado de Pacientes</h2>
+    <div className="w-1/2 lg:w-3/5 md:h:screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
     </div>
   )
 }
